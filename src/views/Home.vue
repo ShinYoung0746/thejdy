30136917f27566ba31b57533d64deef481fd6887
ShinYoung0746 <goddragon@the-jdy.com>
2025-02-07 17:30:25 +0900

    feat:홈 메뉴 추가

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <TheWelcome />
+    <section>
+      <h1>회사 소개</h1>
+      <p>우리 회사는 최고의 서비스를 제공합니다. 고객 만족을 최우선으로 생각하며, 지속적인 혁신을 통해 성장하고 있습니다.</p>
+    </section>
+  </main>
+</template>
