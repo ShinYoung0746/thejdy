808ad8be065cbf2fbe16e901d878ec6483cf4853
ShinYoung0746 <goddragon@the-jdy.com>
2025-02-07 17:52:57 +0900

    feat:홈 메뉴 추가

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,8 @@
-/// <reference types="vite/client" />
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-} 
+interface ImportMetaEnv {
+    readonly VITE_API_KEY: string;
+    readonly VITE_BASE_URL: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
---
+++ src/shims-vue.d.ts
@@ -0,0 +1,31 @@
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '*.svg' {
+  const content: any
+  export default content
+}
+
+declare module '*.png' {
+  const content: any
+  export default content
+}
+
+declare module '*.jpg' {
+  const content: any
+  export default content
+}
+
+declare module '*.jpeg' {
+  const content: any
+  export default content
+}
+
+declare module '*.gif' {
+  const content: any
+  export default content
+} 
