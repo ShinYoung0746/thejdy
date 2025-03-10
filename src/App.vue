70b483e74a5b840ef92e73838215c5275df91ef4
ShinYoung0746 <goddragon@the-jdy.com>
2025-03-10 17:49:39 +0900

    feat: 네이버 검색

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <header>
       <!-- 공통 헤더 내용 -->
-      <meta name="naver-site-verification" content="0f3c7bde77c508a5d2f75e04954fe264a22739d1" />
     </header>
     <router-view />
     <footer>
